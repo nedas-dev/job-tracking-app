842bf8d7ca90dc10da7f6701e5355fd24516bf98
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-04-14 21:51:13 -0700

    sidebar update

--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,15 @@ document.getElementById('sidebar-icon').addEventListener('click', (e) => {
         sidebarLines[0].className = 'sidebar';
         sidebarLines[1].className = 'sidebar';
         sidebarLines[2].className = 'sidebar';
+        document.querySelector('div.sidebar-container').style.transform = "translate(-200px)"
     }
     else {
         sidebarLines[0].className = 'sidebar sidebar1';
         sidebarLines[1].className = 'sidebar sidebar2';
         sidebarLines[2].className = 'sidebar sidebar3';
+        document.querySelector('div.sidebar-container').style.transform = "translate(0px)"
     }
+
+
 
 })
