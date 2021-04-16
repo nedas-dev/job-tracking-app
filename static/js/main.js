b227b1027d2d93b4afcb49d9b733d8f7a4711555
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-04-15 18:27:09 -0700

    sidebar completed

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,48 @@
+class WebsiteControler {
+    constructor() {
+        this.sidebarActive = false
+    }
+}
+
+const sidebarLines = document.querySelector('span.side-bar-container').children;
+const sidebarContainer = document.querySelector('div.sidebar-container');
+const mainContent = document.getElementById('content');
+const websiteController = new WebsiteControler();
+
+
+
+const closeSidebar = function () {
+    sidebarLines[0].className = 'sidebar';
+    sidebarLines[1].className = 'sidebar';
+    sidebarLines[2].className = 'sidebar';
+    sidebarContainer.style.transform = "translate(-200px)";
+
+    websiteController.sidebarActive = false
+
+    mainContent.removeEventListener('click', (e) => {
+        closeSidebar()
+    })
+}
+
+const openSidebar = function () {
+    sidebarLines[0].className = 'sidebar sidebar1';
+    sidebarLines[1].className = 'sidebar sidebar2';
+    sidebarLines[2].className = 'sidebar sidebar3';
+    sidebarContainer.style.transform = "translate(0px)"
+
+    websiteController.sidebarActive = true
+
+    mainContent.addEventListener('click', (e) => {
+        closeSidebar()
+    })
+
+}
+
 document.getElementById('sidebar-icon').addEventListener('click', (e) => {
-    var sidebarLines = document.querySelector('span.side-bar-container').children
-    if (sidebarLines[0].className == 'sidebar sidebar1') {
-        sidebarLines[0].className = 'sidebar';
-        sidebarLines[1].className = 'sidebar';
-        sidebarLines[2].className = 'sidebar';
-        document.querySelector('div.sidebar-container').style.transform = "translate(-200px)"
+    if (websiteController.sidebarActive) {
+        closeSidebar()
     }
     else {
-        sidebarLines[0].className = 'sidebar sidebar1';
-        sidebarLines[1].className = 'sidebar sidebar2';
-        sidebarLines[2].className = 'sidebar sidebar3';
-        document.querySelector('div.sidebar-container').style.transform = "translate(0px)"
+        openSidebar()
     }
-
-
-
 })
