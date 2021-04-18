cc00901ae5d2195e409b7079db157677cd0dbd96
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-04-17 20:27:54 -0700

    secret key hidden

--- JobTrackingProject/settings.py
+++ JobTrackingProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "+i!s=#i3t_b-83jwpmb!d82fa78ft*4-^od_i-sltltc)i0a75"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
