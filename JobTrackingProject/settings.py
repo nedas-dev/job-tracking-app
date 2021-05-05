98f8ac08762de9c38b26da69f33efe7380de6cb1
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-05-04 21:49:32 -0700

    updated

--- JobTrackingProject/settings.py
+++ JobTrackingProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 LOGIN_REDIRECT_URL = "index"
 LOGOUT_REDIRECT_URL = "login"
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
