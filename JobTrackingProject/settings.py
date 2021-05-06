f2a847e399953b41b99544012c9682a7d09b9b4a
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-05-05 20:48:35 -0700

    ..

--- JobTrackingProject/settings.py
+++ JobTrackingProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
