30fabf7f9c68ed5e30ba3ae718f1d08030df3f96
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-05-26 20:50:38 -0700

    added some testing for home page view

--- JobTrackingApp/tests.py
+++ JobTrackingApp/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from django.contrib.auth.models import User
+from . import views
 
 # Create your tests here.
+class IndexTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user(username='nedukas1998', password='nedukas111')
+    
+    def setUp(self):
+        ''' Both login ways are valid! 
+        self.client.login(username='nedukas1998', password='nedukas111')
+        '''
+
+        self.client.post(reverse('login'), {'username':'nedukas1998', 'password': 'nedukas111'})
+
+    def test_home_view_status_code(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+    
+    def test_home_view_template_names(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEquals(response.templates[0].name, 'JobTrackingApp/index.html')
+        self.assertEquals(response.templates[1].name, 'JobTrackingApp/base.html')
+    
+    def test_home_view_func_name(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        # response.resolver_match is equal to resolve(url)
+        self.assertEquals(response.resolver_match.func, views.index)
+        self.assertEquals(response.resolver_match.view_name, 'index')
